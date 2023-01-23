bc6013e30dfd45a6c840e746dfe185cdc56efb88
lonebots <jishnusurajila@gmail.com>
2023-01-23 13:39:41 +0530

    fix:renamed file, typo correction and build success
    fix: typo correction

--- src/turtlesim_ctall/setup.py
+++ src/turtlesim_ctall/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'turtilesim_ctall'
+package_name = 'turtlesim_ctall'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            
+
         ],
     },
 )
