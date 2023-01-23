75364de0c36b223f4c0028cd5510d21d471acd87
lonebots <jishnusurajila@gmail.com>
2023-01-23 17:00:47 +0530

    chore:added dep and executables

--- src/turtlesim_ctall/setup.py
+++ src/turtlesim_ctall/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-
+            "turtle_controller = turtlesim_ctall.turtle_controller_node:main"
         ],
     },
 )
